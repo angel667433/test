c0d60d25ed2f8561d6283d3a912a93cf1ceca3a3
angel667433 <angel667433@csu.fullerton.edu>
2026-09-03 15:17:36 +0000

    finished assignment 2

---
+++ M1A2.py
@@ -0,0 +1,7 @@
+# name: Angel Mendez
+# Student ID 823650395
+# Section nunber: 7
+# Assignment name: Module 1 Assignment 2
+
+name_str = input("What is your name? ")
+print(f"It's dangerous to go alone {name_str}, take this!")

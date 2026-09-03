5dcb3be3a247e2f2ca82c5276294eb9c0241b815
angel667433 <angel667433@csu.fullerton.edu>
2026-09-03 15:48:16 +0000

    created assignment 4

---
+++ M1A4.py
@@ -0,0 +1,14 @@
+# Name: Angel Mendez
+# Student ID 823650395
+# Section: 18254
+# Assignment: Module 1 Assignment 4
+
+catchphrase_str = str(input("Please enter a catchphrase: "))
+message_str = f"[{catchphrase_str.upper()}]"
+print(message_str)
+message_str = f"[{catchphrase_str.lower()}]"
+print(message_str)
+message_str = f"[{catchphrase_str.lstrip()}]"
+print(message_str)
+message_str = f"[{catchphrase_str.rstrip()}]"
+print(message_str)

25fa464097b8ddb88c0060bfd1f1b8e391ebf970
angel667433 <angel667433@csu.fullerton.edu>
2026-09-17 15:37:27 +0000

    finished work 3 module 3

---
+++ M3A3.py
@@ -0,0 +1,11 @@
+# Name: Angel Mendez
+# Student ID 823650395
+# Section: 18254
+# Assignment: Module 3 Assignment 3
+
+integer = int(input("Please enter an integer: "))
+
+if (integer % 2) == 0:
+   print("Even")
+else:
+   print("Odd")

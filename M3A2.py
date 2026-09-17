0cc11ccffb4f1910d09447b8b7beaa3907232cd5
angel667433 <angel667433@csu.fullerton.edu>
2026-09-17 15:33:46 +0000

    finished work 2 module 3

---
+++ M3A2.py
@@ -0,0 +1,12 @@
+# Name: Angel Mendez
+# Student ID 823650395
+# Section: 18254
+# Assignment: Module 3 Assignment 2
+
+m_list = ["E.T. the Extra-Terrestrial", "Indiana Jones and the Last Crusade", "Batman", "Rain Man", "Back to the Future Part II"]
+favorite = input("What is your favorite movie? ")
+
+if favorite in m_list:
+    print("Great choice")
+else:
+    print("Never heard of it")
